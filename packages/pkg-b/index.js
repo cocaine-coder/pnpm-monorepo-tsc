792a2983bcd49b37cd17f932fa318de62c154bac
cocaine_coder <tracywang1997@outlook.com>
2023-11-09 20:41:33 +0800

    update

---
+++ packages/pkg-b/index.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.avg = void 0;
+var pkg_a_1 = require("pkg-a");
+function avg(arr) {
+    if (arr.length < 1)
+        return undefined;
+    return arr.reduce(function (p, c) { return (0, pkg_a_1.sum)(p, c); }, 0) / arr.length;
+}
+exports.avg = avg;
